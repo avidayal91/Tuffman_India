8696392c758b870fadb7a99716e2b161baa37cd0
Avii Dayal <avidayal91@gmail.com>
2018-10-02 17:43:11 +0530

    website changes updates

--- js/main.js
+++ js/main.js
@@ -1,11 +1,3 @@
-$(window).on("scroll", function(){
-    if($(window).scrollTop()){
-       $('header').addClass('white');
-       }
-    else{
-        $('header').removeClass('white');
-    }
-})
 
 $('.owl-carousel').owlCarousel({
     loop:true,
